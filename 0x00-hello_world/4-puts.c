6b9e2ef6e48f076efc5991d1bf08332d26348239
Ogo Nelson <goldnelsgroup@gmail.com>
2023-07-13 15:16:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints out Programming is like building a multilingual puzzle using puts function
+ * Return: 0 (means successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
