b7fe55bd3ef8e44d8ae0abbdc52246c9f6c9e1b1
Ogo Nelson <goldnelsgroup@gmail.com>
2023-07-19 11:53:04 -0700

    sum of numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - summation of integers
+ * @a: first integer
+ * @b: second integer
+ * Return: value of summation
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
